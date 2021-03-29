8f22df0b281a70c07900f09f42907b7ecae96ece
Cristian Yepes <ycristian@uninorte.edu.co>
2021-03-29 15:05:04 -0500

    rspec and factoryBot implemented

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    title { "Sample title" }
+    content { "Sample content" }
+    slug { "sample-article" }
   end
 end
