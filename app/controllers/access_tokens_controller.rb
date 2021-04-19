7d6a82bb129c8763c65dd339644cce9dfe4cf490
Cristian Yepes <ycristian@uninorte.edu.co>
2021-04-19 15:50:05 -0500

    changes until video 46

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,2 +1,12 @@
 class AccessTokensController < ApplicationController
+  def create
+    authenticator = UserAuthenticator.new(params[:code])
+    authenticator.perform
+
+    render json: authenticator.access_token, status: :created
+  end
+
+  def destroy
+    raise AuthorizationError
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
 class ApplicationController < ActionController::API
-  include JsonapiErrorsHandler
+  class AuthorizationError < StandardError; end
 
-  ErrorMapper.map_errors!(
-    'ActiveRecord::RecordNotFound' =>
-      'JsonapiErrorsHandler::Errors::NotFound'
-  )
-  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+
+  rescue_from AuthorizationError, with: :authorization_error
+
+  private
+
+  def authorization_error
+    error = {
+    "status" => "403",
+    "source" => { "pointer" => "/headers/authorization" },
+    "title" =>  "Not authorized",
+    "detail" => "You have no right to access this resource."
+    }
+    render json: { "errors": [ error ] }, status: 403
+  end
+  
+  def authorization_error
+    error = {
+      "status" => "403",
+      "source" => { "pointer" => "/headers/authorization" },
+      "title" =>  "Not authorized",
+      "detail" => "You have no right to access this resource."
+    }
+    render json: { "errors": [ error ] }, status: 403
+  end
 end
